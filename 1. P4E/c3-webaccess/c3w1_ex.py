93809606338e9cca798276f1f3254e70733033d5
Piotr Cieslak <eu.piotr@gmail.com>
2021-09-10 21:28:26 +0200

    regexp exercise update: read file, calc int sum

--- 1. P4E/c3-webaccess/c3w1_ex.py
+++ 1. P4E/c3-webaccess/c3w1_ex.py
@@ -2,37 +2,37 @@
 # input/regex_sum_42.txt --> There are 69 values and the sum ends with 973
 import re
 
-def solution_2_calc_numbers_from_file(filename):
-    try:
-        file = open(filename)
-    except:
-        print('file error')
-        quit()
-
+def solution_1_calc_numbers_from_file(filename):
+    file = open(filename)
     ints = []
+
     for line in file:
         strings_list = re.findall('[\d]+', line)
-
         for word in strings_list:
             ints.append(int(word))
-    print('#' + str(len(int_values)) + ': ' + str(sum(int_values)))
+    print('#' + str(len(ints)) + ': ' + str(sum(ints)))
 
 
-def solution_1_calc_numbers_from_file(filename):
-    try:
-        file = open(filename)
-    except:
-        print('file error')
-        quit()
-
+def solution_2_calc_numbers_from_file(filename):
+    file = open(filename)
     values = re.findall('[\d]+', file.read())
-
     ints = []
+
     for v in values:
         ints.append(int(v))
     print("#" + str(len(values)) + ": " + str(sum(ints)))
+
+
+def best_solution(fn):
+    fh = open(fn, 'r')
+
+    # list comprehension: x is every digit in string; make it int; create a list;
+    ints = [int(x) for x in re.findall('[\d]+', fh.read())]
+    print('#' + str(len(ints)) + ': ' + str(sum(ints)))
+
 
 if __name__ == "__main__":
     name = "input/regex_sum_1353131.txt"
     solution_1_calc_numbers_from_file(name)
     solution_2_calc_numbers_from_file(name)
+    best_solution(name)
