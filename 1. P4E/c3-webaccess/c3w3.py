7522e46584cf68eee365f27aa3dc9104010d3e5b
Piotr Cieslak <eu.piotr@gmail.com>
2021-09-11 17:26:05 +0200

    web access: sockets, urrlib, beautifulsoup --> usage

--- 1. P4E/c3-webaccess/c3w3.py
+++ 1. P4E/c3-webaccess/c3w3.py
@@ -1,16 +1,38 @@
 # web communication
+# sockets, urllib
 import socket
+import urllib.request, urllib.parse, urllib.error
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+def get_file_using_socket():
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock.connect(('data.pr4e.org', 80))
+    cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+    mysock.send(cmd)
 
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode(),end='')
+    while True:
+        data = mysock.recv(512)
+        if len(data) < 1:
+            break
+        print(data.decode(),end='')
 
-mysock.close()
+    mysock.close()
 
+def get_file_using_urllib():
+    # one option
+    file = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+    for line in file:
+        print(line.decode().strip())
+
+    print('\n--- separator ---\n')
+
+    # second option
+    file2 = urllib.request.urlopen('http://data.pr4e.org/romeo.txt').read().decode()
+    print(file2)
+
+
+def call_main():
+    # get_file_using_socket()
+    get_file_using_urllib()
+
+if __name__ == "__main__":
+    call_main()
---
+++ 1. P4E/c3-webaccess/c3w4.py
@@ -0,0 +1,11 @@
+# beautiful soup
+from bs4 import BeautifulSoup
+
+
+def get_file_using
+
+def call_main():
+    print('hi')
+
+if __name__ == "__main__":
+    call_main()
