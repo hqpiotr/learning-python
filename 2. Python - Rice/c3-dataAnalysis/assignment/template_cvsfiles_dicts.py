0e642bca60f6eb6906fd037cb80cc9a3615832eb
Piotr Cieslak <eu.piotr@gmail.com>
2021-09-24 21:43:16 +0200

    RICE: CSV DictReader

---
+++ 2. Python - Rice/c3-dataAnalysis/assignment/template_cvsfiles_dicts.py
@@ -0,0 +1,183 @@
+"""
+Project for Week 3 of "Python Data Analysis".
+Read and write CSV files using a dictionary of dictionaries.
+
+Be sure to read the project description page for further information
+about the expected behavior of the program.
+"""
+import collections
+import csv
+
+def read_csv_fieldnames(filename, separator, quote):
+    """
+    Inputs:
+      filename  - name of CSV file
+      separator - character that separates fields
+      quote     - character used to optionally quote fields
+    Ouput:
+      A list of strings corresponding to the field names in
+      the given CSV file.
+    """
+    with open(filename, "rt", newline='') as file:
+        csvreader = csv.DictReader(file,
+                                   skipinitialspace=False,
+                                   delimiter=separator,
+                                   quotechar=quote)
+        headers = list(csvreader.fieldnames)
+
+    # print(headers)
+    return headers
+
+
+def read_csv_as_list_dict(filename, separator, quote):
+    """
+    Inputs:
+      filename  - name of CSV file
+      separator - character that separates fields
+      quote     - character used to optionally quote fields
+    Output:
+      Returns a list of dictionaries where each item in the list
+      corresponds to a row in the CSV file.  The dictionaries in the
+      list map the field names to the field values for that row.
+    """
+    list_of_dicts = []
+    with open(filename, "rt", newline='') as file:
+        csvreader = csv.DictReader(file, skipinitialspace=False,
+                                   delimiter=separator,
+                                   quotechar=quote)
+
+        for _ in csvreader:
+            nested_row_dict = {}
+
+            # print(line)
+            # row0  field1: val1, field2: val2
+            # row1  field1: val3, field2: val4
+            # nested_row_dict = collections.OrderedDict()
+            # OK: WORKING 4 lines, but commented
+            for col in range(len(csvreader.fieldnames)):
+                nested_row_dict[csvreader.fieldnames[col]] =\
+                    nested_row_dict.get(csvreader.fieldnames[col], col)
+            list_of_dicts.append(nested_row_dict)
+    return list_of_dicts
+
+"""
+ERROR:
+[-20.0 pts] read_csv_as_list_dict('table1.csv', ',', '"') expected 
+[OrderedDict([('Field1', '1'), ('Field2', '2'), ('Field3', '3'), ('Field4', '4')]), 
+OrderedDict([('Field1', '5'), ('Field2', '6'), ('Field3', '7'), ('Field4', '8')]), 
+OrderedDict([('Field1', '9'), ('Field2', '10'), ('Field3', '11'), ('Field4', '12')])] but received [{'Field1': 0, 'Field2': 1, 'Field3': 2, 'Field4': 3}, {'Field1': 0, 'Field2': 1, 'Field3': 2, 'Field4': 3}, {'Field1': 0, 'Field2': 1, 'Field3': 2, 'Field4': 3}]
+but received 
+[
+    {'Field1': 0, 'Field2': 1, 'Field3': 2, 'Field4': 3},
+    {'Field1': 0, 'Field2': 1, 'Field3': 2, 'Field4': 3}, 
+    {'Field1': 0, 'Field2': 1, 'Field3': 2, 'Field4': 3}]
+"""
+
+# TODO: not needed
+def print_list_of_dicts(table):
+    for row, value in enumerate(table):
+        print("row", row, end=':\t')
+        for k, v in value.items():
+            print(k, ":", v, sep='', end=' ')
+        print()
+
+def print_list_of_dicts_v2(table):
+    for row in table:
+        print(row)
+
+
+
+def read_csv_as_nested_dict(filename, keyfield, separator, quote):
+    """
+    Inputs:
+      filename  - name of CSV file
+      keyfield  - field to use as key for rows
+      separator - character that separates fields
+      quote     - character used to optionally quote fields
+    Output:
+      Returns a dictionary of dictionaries where the outer dictionary
+      maps the value in the key_field to the corresponding row in the
+      CSV file.  The inner dictionaries map the field names to the
+      field values for that row.
+    """
+    big_dict = {}
+    with open(filename, "rt") as file:
+        csvreader = csv.DictReader(file,
+                                   skipinitialspace=False,
+                                   delimiter=separator,
+                                   quotechar=quote)
+        for line in csvreader:
+            big_dict[line[keyfield]] = line
+    # print(big_dict)
+    return big_dict
+
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr',
+          'May', 'Jun', 'Jul', 'Aug',
+          'Sep', 'Oct', 'Nov', 'Dec')
+
+
+# TODO: testing
+def print_nested_dict(table):
+    print("City                ", end='')
+    for m in MONTHS:
+        print(f'{m:>4}', end='')
+    print('\n' + '-' * 64)
+
+    for key, value in table.items():
+        print(f'{key:<20}', end='')
+        for m in MONTHS:
+            print(f'{value[m]:>4}', end='')
+        print("")
+
+
+def write_csv_from_list_dict(filename, table, fieldnames, separator, quote):
+    """
+    Inputs:
+      filename   - name of CSV file
+      table      - list of dictionaries containing the table to write
+      fieldnames - list of strings corresponding to the field names in order
+      separator  - character that separates fields
+      quote      - character used to optionally quote fields
+    Output:
+      Writes the table to a CSV file with the name filename, using the
+      given fieldnames.  The CSV file should use the given separator and
+      quote characters.  All non-numeric fields will be quoted.
+    """
+    with open(filename, 'w', newline='') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames, delimiter=separator,
+                                    quotechar=quote, quoting=csv.QUOTE_NONNUMERIC)
+        csv_writer.writeheader()
+        for row in table:
+            # csv_writer.writerow(table[row])
+            csv_writer.writerow(row)
+
+
+# Testing #1
+# read_csv_fieldnames('table1.csv', ',', '"')
+# read_csv_fieldnames('table2.csv', ',', '"')
+# read_csv_fieldnames('table3.csv', ',', "'")
+# read_csv_fieldnames('table4.csv', ',', '"')
+# read_csv_fieldnames('table5.csv', ',', '"')
+
+
+# Testing #2
+# t_l_d = read_csv_as_list_dict('hightemp.csv', ',', '"')
+# print_list_of_dicts(t_l_d)
+# print_list_of_dicts_v2(t_l_d)
+# t_l_d = read_csv_as_list_dict('table2.csv', ',', '"')
+# t_l_d = read_csv_as_list_dict('table3.csv', ',', "'")
+# t_l_d = read_csv_as_list_dict('table4.csv', ',', '"')
+# t_l_d = read_csv_as_list_dict('table5.csv', ',', '"')
+# print_list_of_dicts_v2(t_l_d)
+# print(t_l_d)
+
+# Testing #3
+# n2d = read_csv_as_nested_dict('hightemp.csv', 'City', ",", '"')
+# print_nested_dict(n2d)
+
+# Testing #4
+name = 'table1.csv'
+fieldnames = read_csv_fieldnames(name, ',', '"')
+mytable = read_csv_as_list_dict(name, ',', '"')
+print(mytable)
+# write_csv_from_list_dict('output.csv', mytable, fieldnames, ',', '"')
--- 2. Python - Rice/c3-dataAnalysis/ex_2.py
+++ 2. Python - Rice/c3-dataAnalysis/ex_2.py
@@ -5,7 +5,10 @@ rows = 5
 cols = 5
 list_of_lists = []
 list_of_dicts = []
+dict_of_lists = {}
+dict_of_dicts = {}
 NAMES = ("piotr", "ola", "bartus", "stas", "nel")
+JOBS = ("engineer", "manager", "director")
 
 def create_list_of_lists(nr, nc):
     for r in range(nr):
@@ -14,17 +17,49 @@ def create_list_of_lists(nr, nc):
             internal_list.append(random.randrange(0,9,1))
         list_of_lists.append(internal_list)
 
-def print_list_of_lists(table):
-    for i in table:
-        print(i)
-
 def create_list_of_dicts(nr, nc):
     for r in range(nr):
         nested_dict = {}
         for c in range(nc):
             nested_dict[NAMES[c]] = nested_dict.get(NAMES[c],
-                                                    random.randrange(5,9,1))
+                                                    random.randrange(0,9,1))
         list_of_dicts.append(nested_dict)
+
+def create_dict_of_lists(nr, nc):
+    for i in range(nr):
+        nested_list = []
+        for c in range(nc):
+            nested_list.append(random.randrange(0,9,1))
+        dict_of_lists[NAMES[i]] = dict_of_lists.get(NAMES[i], nested_list)
+
+def create_dict_of_dicts(nr, nc):
+    for i in range(nr):
+        nested_dict = {}
+        for j in range(nc):
+            nested_dict[NAMES[j]] = nested_dict.get(NAMES[j],
+                                                    random.randrange(0,9,1))
+
+        dict_of_dicts[JOBS[i]] = dict_of_dicts.get(JOBS[i], nested_dict)
+
+def print_dict_of_dicts(table):
+    print("\n{")
+
+    for k in table.keys():
+        print("\t\t" + k + ":")
+        for v in table.values():
+            print("\t\t\t" + str(v), end=' ')
+            print()
+
+    print("}")
+
+
+def print_dict_of_lists(table):
+    for k in table:
+        print("[" + k + "]: ", table[k], end='\n')
+
+def print_list_of_lists(table):
+    for i in table:
+        print(i)
 
 def print_list_of_dicts_v1(table):
     for i in table:
@@ -40,15 +75,23 @@ def print_list_of_dicts_v2(table):
 
 
 """ testing """
+""" 1 """
 create_list_of_lists(3, 3)
 print(list_of_lists, "\n")
 print_list_of_lists(list_of_lists)
 print()
+""" 2 """
 create_list_of_dicts(3, 3)
 print(list_of_dicts, "\n")
 print_list_of_dicts_v1(list_of_dicts)
-
-# tell me what has piotr is second row
-# print(list_of_dicts[2]['bartus'])
 print_list_of_dicts_v2(list_of_dicts)
 print(list_of_dicts.pop())
+""" 3 """
+create_dict_of_lists(3,3)
+print(dict_of_lists, "\n")
+print_dict_of_lists(dict_of_lists)
+""" 4 """
+create_dict_of_dicts(3,3)
+print(dict_of_dicts)
+print_dict_of_dicts(dict_of_dicts)
+print(dict_of_dicts['director']['bartus'])
