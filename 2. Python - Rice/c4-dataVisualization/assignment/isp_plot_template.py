8b4ba9944710a69928153b65425c61778ed21352
Piotr Cieslak <eu.piotr@gmail.com>
2021-09-28 15:12:40 +0200

    RICE w3 assignment

--- 2. Python - Rice/c4-dataVisualization/assignment/isp_plot_template.py
+++ 2. Python - Rice/c4-dataVisualization/assignment/isp_plot_template.py
@@ -1,14 +1,49 @@
 """
 Project for Week 2 of "Python Data Visualization".
 Read World Bank GDP data and create some basic XY plots.
-
 Be sure to read the project description page for further information
 about the expected behavior of the program.
 """
-
 import csv
 import pygal
+import random
 
+def print_some_examples_from_csv(sourcetable, amount, country=None):
+    """
+    Function takes the table with sample amount of countries to print
+    :param sourcetable: dictionary input
+    :param amount: number of countries to sample printout
+    :param country: optional, for printing only one country if needed
+    :return: nothing is returned, printing function only
+    """
+    countries_counter = 0
+    for key, value in sourcetable.items():
+        output = ('{:<20} {:>5} {:>5} {:>15} {:>12} {:>12} {:>12} {:>12} {:>12} {:>12}'.format(key,
+                  value['Country Code'],
+                  value['Indicator Name'],
+                  value['Indicator Code'],
+                  value['2000'],
+                  value['2001'],
+                  value['2002'],
+                  value['2003'],
+                  value['2004'],
+                  value['2005']))
+        if country is None and countries_counter < amount:
+            print(output)
+            countries_counter += 1
+        elif key == country:
+            print(output)
+            break
+    print("-" * 138)
+
+def print_tuples_nl(data):
+    """
+    Function to print tuple () data only
+    :param data: input source as pair of tuples
+    :return: nothing, printing only
+    """
+    for item in data:
+        print("\t( " + str(item[0]) + ", " + str(item[1]) + " )")
 
 def read_csv_as_nested_dict(filename, keyfield, separator, quote):
     """
@@ -31,8 +66,6 @@ def read_csv_as_nested_dict(filename, keyfield, separator, quote):
             result[line[keyfield]] = line
     return result
 
-
-
 def build_plot_values(gdpinfo, gdpdata):
     """
     Inputs:
@@ -41,14 +74,23 @@ def build_plot_values(gdpinfo, gdpdata):
                 keys are strings indicating a year and whose values
                 are strings indicating the country's corresponding GDP
                 for that year.
-
-    Output: 
+    Output:
       Returns a list of tuples of the form (year, GDP) for the years
       between "min_year" and "max_year", inclusive, from gdpinfo that
       exist in gdpdata.  The year will be an integer and the GDP will
       be a float.
     """
-    return []
+    # years = [x for x in range(int(gdpinfo['min_year']), int(gdpinfo['max_year']) + 1)]
+    years = list(range(int(gdpinfo['min_year']), int(gdpinfo['max_year']) + 1))
+    tuples = []
+
+    for country in gdpdata:
+        if country != 'Not classified':
+            for year in years:
+                if country == str(year):
+                    if gdpdata[country] != '':
+                        tuples.append((int(year), float(gdpdata[country])))
+    return tuples
 
 
 def build_plot_dict(gdpinfo, country_list):
@@ -66,7 +108,21 @@ def build_plot_dict(gdpinfo, country_list):
       CSV file should still be in the output dictionary, but
       with an empty XY plot value list.
     """
-    return {}
+    gdp_data = read_csv_as_nested_dict(gdpinfo['gdpfile'],
+                                       gdpinfo['country_name'],
+                                       gdpinfo['separator'],
+                                       gdpinfo['quote'])
+    dict_country_coords = {}
+
+    for country in country_list:
+        dict_country_coords[country] = []
+        for key, value in gdp_data.items():
+            if key == country:
+                tup = build_plot_values(gdpinfo, gdp_data[country])
+                dict_country_coords[country] = tup
+            else:
+                continue
+    return dict_country_coords
 
 
 def render_xy_plot(gdpinfo, country_list, plot_file):
@@ -86,43 +142,50 @@ def render_xy_plot(gdpinfo, country_list, plot_file):
     """
     return
 
-def print_some_examples_from_csv(sourcetable, amount):
-    countries_counter = 0
-    columns_counter = 5
-    for key, value in sourcetable.items():
-        columns_counter += 1
-        if countries_counter < amount:
-            # print(str('{0}') + '{}' + ':' + ' {} ' + ' {} ' + ' {} ' + ' {} '.format(
-            print('{:<20} {:>5} {:>5} {:>15} {:>12} {:>12} {:>12} {:>12} {:>12} {:>12}'.format(key,
-                                 value['Country Code'],
-                                 value['Indicator Name'],
-                                 value['Indicator Code'],
-                                 value['2000'],
-                                 value['2001'],
-                                 value['2002'],
-                                 value['2003'],
-                                 value['2004'],
-                                 value['2005']))
-            countries_counter += 1
-
-
 def test_render_xy_plot():
     """
     Code to exercise render_xy_plot and generate plots from
     actual GDP data.
     """
-    gdpinfo = {
-        "gdpfile": "isp_gdp.csv",
-        "separator": ",",
-        "quote": '"',
-        "min_year": 1960,
-        "max_year": 2015,
-        "country_name": "Country Name",
-        "country_code": "Country Code"
-    }
-    csvdataread = read_csv_as_nested_dict(gdpinfo['gdpfile'], 'Country Name', gdpinfo['separator'], gdpinfo['quote'])
-    print_some_examples_from_csv(csvdataread, 10)
+    # gdpinfo = {
+    #     "gdpfile": "isp_gdp.csv",
+    #     "separator": ",",
+    #     "quote": '"',
+    #     "min_year": 1960,
+    #     "max_year": 2015,
+    #     "country_name": "Country Name",
+    #     "country_code": "Country Code"
+    # }
+    # SAMPLE_NUMBER_OF_COUNTRIES = 1
+    # SAMPLE_NUMBER_OF_YEAR_TUPLES = 10
+    # 1:
+    # csvdata = read_csv_as_nested_dict(gdpinfo['gdpfile'], 'Country Name', gdpinfo['separator'], gdpinfo['quote'])
+    # countries_only_csvdata = list(csvdata.keys())
+    # random.shuffle(countries_only_csvdata)
+    # countries_only_csvdata = countries_only_csvdata[:SAMPLE_NUMBER_OF_COUNTRIES]
+    # print(countries_only_csvdata)
+    # print_some_examples_from_csv(csvdata, 2)
+    # print_some_examples_from_csv(csvdata, 1, 'Poland')
 
+    # 2:
+    # tuples = build_plot_values(gdpinfo, csvdata[countries_only_csvdata[0]])
+    # tuples = build_plot_values(gdpinfo, csvdata['Poland'])
+    # print(tuples)
+    # print("-" * 100 + "\n" + countries_only_csvdata[0])
+    # print_tuples_nl(tuples[:SAMPLE_NUMBER_OF_YEAR_TUPLES])
+
+    # 3:
+    # plot_dict = build_plot_dict(gdpinfo, csvdata[countries_only_csvdata[0]])
+    # plot_dict = build_plot_dict(gdpinfo, ['Poland'])
+    # print(plot_dict)
+
+    # TEST:
+    # build_plot_dict(
+    # {'gdpfile': 'gdptable1.csv', 'separator': ',', 'quote': '"', 'min_year': 2000, 'max_year': 2005,
+    #                  'country_name': 'Country Name', 'country_code': 'Code'},
+    #                  ['Country3'])
+    # expected     {'Country3': []}
+    # but     received     {}
 
     # Auto-generated
     # render_xy_plot(gdpinfo, [], "isp_gdp_xy_none.svg")
@@ -134,5 +197,5 @@ def test_render_xy_plot():
 # Make sure the following call to test_render_xy_plot is commented out
 # when submitting to OwlTest/CourseraTest.
 
-test_render_xy_plot()
+#test_render_xy_plot()
 
